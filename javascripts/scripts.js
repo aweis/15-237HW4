d230a87a593d6a9dd55029bc562edd94fda09642
Maria Khutoretsky <mkhutore@andrew.cmu.edu>
2012-10-03 19:45:49 -0400

    making new lists plus jquery

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -7,6 +7,7 @@
 
 // Gets the reference to the task form and saves it in a variable
 var taskForm = document.getElementById('task_form');
+var listForm = document.getElementById("list_form");
 
 // Attaching the submit event to the form
 // Different browsers do it differently so we include both ways
@@ -16,20 +17,28 @@ if (taskForm.attachEvent) {
   taskForm.addEventListener("submit", processForm); 
 }
 
+if(listForm.attachEvent) {
+	listForm.attachEvent("submit", processListForm);
+} else {
+	listForm.addEventListener("submit", processListForm);
+}
+
 function processForm(e) {
   // By default, the HTML will submit by POSTing to another page.
   // We prevent this behavior
   // e is the event argument passed into the function
   if (e.preventDefault) e.preventDefault();
-  
   var formData = new Object();
-  formData['new_task'] = document.getElementById('new_task').value;
+	var form = $($(e.target).children()[0]).val();
+	formData["input"] = form;
+
+	var list = $(e.target.parentNode).children()[0];
 
   //Perform validation
   if (validateForm(formData)) {
     console.log('Validation passed');
-    updateList(formData);
-    clearForm();
+    updateList(formData, $(list));
+    clearForm(e.target);
   } else {
     console.log('Validation failed');
   }
@@ -38,34 +47,91 @@ function processForm(e) {
   return false;
 }
 
+function processListForm(e) {
+	// By default, the HTML will submit by POSTing to another page.
+	// We prevent this behavior
+	// e is the event argument passed into the function
+	if (e.preventDefault) e.preventDefault();
+	var formData = new Object();
+	formData["input"] = document.getElementById("new_list").value;
+	
+	//Perform validation
+	if (validateForm(formData)) {
+		console.log('Validation passed');
+		newList(formData);
+		clearForm(listForm);
+	} else {
+		console.log('Validation failed');
+	}
+	// You must return false to prevent the default form submit behavior
+	return false;
+}
+
 /*
 Return true if the form is valid, and false otherwise
 
 */
 function validateForm(formData) {
-  var task = formData['new_task'];
-  if(typeof task === "string" && task !== "") {
-    console.log("Validation passed on task: " + task);
+  var input = formData['input'];
+  if(typeof input === "string" && input !== "") {
+    console.log("Validation passed on task: " + input);
   } else {
-    console.log("Validation failed on task: " + task);
+    console.log("Validation failed on task: " + input);
     return false;
   }
   return true;
 }
 
-function updateList(formData) {
-  var todoList = document.getElementById('todoList');
-  var task = formData['new_task'];
-  var taskText = document.createTextNode(task);
-  var item = document.createElement('li');
-  item.appendChild(taskText);
-  todoList.appendChild(item);
+function updateList(formData, list) {
+  var task = formData['input'];
+  var taskText = $(document.createTextNode(task));
+  var item = $("<li/>");
+  item.append(taskText);
+  list.append(item);
+}
+
+function newList(formData) {
+	var listList = $(document.getElementById("lists"));
+	var newL = formData["input"];
+	
+	var listName = $("<div>" + newL + "</div>");
+	listName.addClass("listName");
+
+	var list = $("<div/>");
+	list.addClass("list");
+
+	var tdl = $("<ul/>");
+	tdl.addClass("toDoList");
+
+	var label = $("<label>Add your task here</label>");
+	label.attr("for", "new_task");
+	var form = $("<form/>");
+	var text = $("<input/>");
+	text.attr("type", "text");
+	var button = $("<input>");
+	button.attr("type", "submit");
+	button.attr("value", "Add");
+
+	form.append(text);
+	form.append(button);
+	form.submit(processForm);
+
+	list.append(tdl);
+	list.append(label);
+	list.append(form);
+
+	var listBox = $("<div/>");
+	listBox.addClass("listBox");
+
+	listBox.append(listName);
+	listBox.append(list);
+	listList.append(listBox);
 }
 
 /*
  * Resets the form and sets the focus back to the first field
 */
-function clearForm() {
-  taskForm.reset();
-  document.getEelementById('new_task').focus();
+function clearForm(form) {
+  form.reset();
+  $(form).children()[0].focus();
 }
